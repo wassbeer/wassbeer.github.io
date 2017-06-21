0a0a753c0eedd8f39ba90ec87c5e87c483ebc5bd
wassbeer <tm.wassenberg@gmail.com>
2017-06-21 13:41:16 +0200

    edited the jquery list to css colour change

--- html-project-3/main.js
+++ html-project-3/main.js
@@ -19,13 +19,15 @@ $(document).ready(function(){
 // function 1 of 2 is declared for when mouse enters
 	function() {
 	// the color property of .nav li will turn red so its text will
-	$(this).fadeOut(100);
+	//$(this).fadeOut(100);
+	$(this).css( "color", "red" );
 	// a comma indicated the border between the first and the second function
 	}, 
 	// a second function is declared
 	function(){
 	// changing the color back to black when mouse leaves
-	$(this).fadeIn(5000);
+	// $(this).fadeIn(5000);
+	$(this).css( "color", "black" );
 	}); 
 
 // "If you sleep 3 hours two nights in a row, stay calm", "let nature take its course", "life is life"
